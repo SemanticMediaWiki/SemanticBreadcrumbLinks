10deb60ebd29ff5f1a516b33663181f07032a408
mwjames <jamesin.hongkong.1@gmail.com>
2016-05-26 00:05:49 +0200

    Update install-semantic-breadcrumb-links.sh
    [skip ci]

--- tests/travis/install-semantic-breadcrumb-links.sh
+++ tests/travis/install-semantic-breadcrumb-links.sh
@@ -12,17 +12,17 @@ function installToMediaWikiRoot {
 
 	if [ "$PHPUNIT" != "" ]
 	then
-		composer require 'phpunit/phpunit='$PHPUNIT --prefer-source --update-with-dependencies
+		composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
 	else
-		composer require 'phpunit/phpunit=3.7.*' --prefer-source --update-with-dependencies
+		composer require 'phpunit/phpunit=3.7.*' --update-with-dependencies
 	fi
 
 	if [ "$SBL" != "" ]
 	then
-		composer require 'mediawiki/semantic-breadcrumb-links='$SBL --prefer-source --update-with-dependencies
+		composer require 'mediawiki/semantic-breadcrumb-links='$SBL --update-with-dependencies
 	else
 		composer init --stability dev
-		composer require mediawiki/semantic-breadcrumb-links "dev-master" --prefer-source --dev
+		composer require mediawiki/semantic-breadcrumb-links "dev-master" --dev
 
 		cd extensions
 		cd SemanticBreadcrumbLinks
